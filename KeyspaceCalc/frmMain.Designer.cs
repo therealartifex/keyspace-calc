f02e0340400502a5b9e15e5d574c33ae145ab4c3
therealartifex <therealartifex@users.noreply.github.com>
2020-11-30 10:23:12 -0500

    initial commit

---
+++ KeyspaceCalc/frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace KeyspaceCalc
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMask = new System.Windows.Forms.TextBox();
+            this.lblKeyspace = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numSpeed = new System.Windows.Forms.NumericUpDown();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.cmbUnit = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMask
+            // 
+            this.txtMask.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMask.Location = new System.Drawing.Point(109, 122);
+            this.txtMask.Name = "txtMask";
+            this.txtMask.Size = new System.Drawing.Size(883, 44);
+            this.txtMask.TabIndex = 0;
+            this.txtMask.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMask.TextChanged += new System.EventHandler(this.txtMask_TextChanged);
+            // 
+            // lblKeyspace
+            // 
+            this.lblKeyspace.AutoSize = true;
+            this.lblKeyspace.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeyspace.Location = new System.Drawing.Point(12, 254);
+            this.lblKeyspace.Name = "lblKeyspace";
+            this.lblKeyspace.Size = new System.Drawing.Size(188, 27);
+            this.lblKeyspace.TabIndex = 1;
+            this.lblKeyspace.Text = "Keyspace = ";
+            this.lblKeyspace.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mask";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Speed";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numSpeed
+            // 
+            this.numSpeed.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numSpeed.Location = new System.Drawing.Point(128, 181);
+            this.numSpeed.Maximum = new decimal(new int[] {
+            -559939585,
+            902409669,
+            54,
+            0});
+            this.numSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSpeed.Name = "numSpeed";
+            this.numSpeed.Size = new System.Drawing.Size(750, 39);
+            this.numSpeed.TabIndex = 4;
+            this.numSpeed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numSpeed.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSpeed.ValueChanged += new System.EventHandler(this.numSpeed_ValueChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Lucida Console", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(12, 312);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(284, 27);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "Exhaustion Time: ";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbUnit
+            // 
+            this.cmbUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnit.Font = new System.Drawing.Font("Lucida Console", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbUnit.Items.AddRange(new object[] {
+            "H/s",
+            "KH/s",
+            "MH/s",
+            "GH/s",
+            "TH/s"});
+            this.cmbUnit.Location = new System.Drawing.Point(884, 180);
+            this.cmbUnit.Name = "cmbUnit";
+            this.cmbUnit.Size = new System.Drawing.Size(108, 40);
+            this.cmbUnit.TabIndex = 7;
+            this.cmbUnit.SelectedIndexChanged += new System.EventHandler(this.cmbUnit_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 450);
+            this.Controls.Add(this.cmbUnit);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.numSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblKeyspace);
+            this.Controls.Add(this.txtMask);
+            this.Name = "frmMain";
+            this.Text = "Keyspace Calculator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMask;
+        private System.Windows.Forms.Label lblKeyspace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numSpeed;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.ComboBox cmbUnit;
+    }
+}
+
---
+++ KeyspaceCalc/frmMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace KeyspaceCalc
+{
+    public partial class frmMain : Form
+    {
+        ulong keyspace = 1;
+        NumberFormatInfo NFI;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Kick()
+        {
+            var chars = txtMask.Text.ToCharArray();
+            var speed = numSpeed.Value;
+            var t = new TimeSpan();
+            string outputKeySpace = "1";
+            string outputTime = "";
+
+            switch (cmbUnit.SelectedIndex)
+            {
+                case 1:
+                    speed *= 1000;
+                    break;
+                case 2:
+                    speed *= 1000000;
+                    break;
+                case 3:
+                    speed *= 1000000000;
+                    break;
+                case 4:
+                    speed *= 1000000000000;
+                    break;
+            }
+
+            for (var i = 0; i < chars.Length; ++i)
+            {
+                if (chars[i] == '?' && i < chars.Length - 1)
+                {
+                    try
+                    {
+                        checked
+                        {
+                            switch (chars[i + 1])
+                            {
+                                case 'a':
+                                    keyspace *= 95;
+                                    break;
+
+                                case 'l':
+                                case 'u':
+                                    keyspace *= 26;
+                                    break;
+
+                                case 's':
+                                    keyspace *= 33;
+                                    break;
+
+                                case 'd':
+                                    keyspace *= 10;
+                                    break;
+
+                                case 'b':
+                                    keyspace *= 256;
+                                    break;
+
+                                case 'h':
+                                case 'H':
+                                    keyspace *= 16;
+                                    break;
+                            }
+                        }
+
+                        outputKeySpace = keyspace.ToString("N0", NFI);
+                        t = TimeSpan.FromSeconds((double)(keyspace / speed));
+                        outputTime = $"{t.Days} days, {t.Hours} hrs, {t.Minutes} min, {t.Seconds} sec";
+                    }
+                    catch (OverflowException ex)
+                    {
+                        if (ex.Message.Contains("TimeSpan"))
+                            outputTime = "Fuck";
+
+                        if (ex.Message.Contains("Arithmetic"))
+                            outputKeySpace = "Fuck";
+                    }
+                }
+            }
+
+            lblKeyspace.Text = $"Keyspace = {outputKeySpace}";
+            lblTime.Text = $"Exhaustion Time: {outputTime}";
+            keyspace = 1;
+        }
+
+        private void txtMask_TextChanged(object sender, EventArgs e)
+        {
+            Kick();
+        }
+
+        private void numSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            Kick();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cmbUnit.SelectedIndex = 0;
+            NFI = new NumberFormatInfo();
+            NFI.NumberGroupSeparator = " ";
+        }
+
+        private void cmbUnit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Kick();
+        }
+    }
+}
